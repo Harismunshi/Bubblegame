b0b5a0f2c9e50dfb4c2017849cfb0411c3582adf
Harismunshi <142170103+Harismunshi@users.noreply.github.com>
2023-10-14 18:51:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore() {
+
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+
+}
+
+function getNewHit() {
+    hitrn = Math.floor(Math.random() * 10)
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+
+    var clutter = "";
+
+    for (var i = 1; i < 170; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${rn}</div>`
+    };
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+
+}
+
+function runTimer() {
+    var timeint = setInterval(function () {
+
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+
+        else {
+            clearInterval(timeint);
+            document.querySelector("#pbtm").innerHTML = `<h1 class = over>Game Over<h1>` + `<h4>Made By : Munshi Harisahmed Ayubkhan</h4>`;
+
+        }
+    }, 1000)
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+
+    var Clickednum = Number(dets.target.textContent);
+    if (Clickednum === hitrn) {
+
+        increaseScore();
+        getNewHit();
+        makeBubble();
+    }
+
+})
+
+
+
+getNewHit();
+makeBubble();
+runTimer();
